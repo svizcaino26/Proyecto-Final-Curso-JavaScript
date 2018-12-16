47206fe210c1922fb8eb325e84312e8f65cbee2b
stephen <svizcaino26@gmail.com>
2018-12-16 09:18:24 -0500

    Update app.js
    added button shrink function
    code documented
    * ready for evaluation

--- js/app.js
+++ js/app.js
@@ -28,26 +28,27 @@ var Calculadora = {
   pendingVal: undefined,
   prevVal: undefined,
   currentOperator: undefined,
+  keyPressed: undefined,
 
-  //Function for handling object Calculadora click events
+  //Function for handling object Calculadora click events and updating display
   updateDisplay: function(clickObj){
     var displayVal = Calculadora.calc_display.innerHTML;
     // var pendingVal;
     // var evalArray = [];
     var numKeyArray = ['0','1','2','3','4','5','6','7','8','9']; //aux array to validate numeric keys
     switch (clickObj.target.id) { //Switch handler for mouse click events
-      case 'on':
-        Calculadora.calc_display.innerHTML = '0';
-        Calculadora.pendingVal = undefined;
-        Calculadora.evalArray = [];
-        Calculadora.prevVal = undefined;
-        Calculadora.currentOperator = undefined;
+      case 'on':    //ON/C key event
+        Calculadora.calc_display.innerHTML = '0'; //sets display to 0
+        Calculadora.pendingVal = undefined; //clears operation pending value
+        Calculadora.evalArray = []; //clears string evaluation array
+        Calculadora.prevVal = undefined; //clears value for cyclic operations
+        Calculadora.currentOperator = undefined; //clears current stored operator for cyclic operations
         break;
 
       case 'punto':
         if (!Calculadora.calc_display.innerHTML.includes('.')) {
           displayVal += '.';
-          if (displayVal.length > 8) {
+          if (displayVal.length > 8) { //validates for 8 digits and decimal point
             displayVal = displayVal.slice(0, 8);
           }
           Calculadora.calc_display.innerHTML = displayVal;
@@ -59,69 +60,73 @@ var Calculadora = {
         minusSign = Number(displayVal);
         minusSign *= -1;
         displayVal = minusSign.toString();
-        if (displayVal.length > 9) {
+        if (displayVal.length > 9) { //validates for 8 digits and sign
           displayVal = displayVal.slice(0, 9);
         }
         Calculadora.calc_display.innerHTML = displayVal;
         break;
 
       case 'mas':
-        Calculadora.currentOperator = '+';
-        Calculadora.pendingVal = displayVal;
-        displayVal = '';
-        Calculadora.evalArray.push(Calculadora.pendingVal);
-        Calculadora.evalArray.push('+');
-        if (displayVal.length > 8) {
+        Calculadora.currentOperator = '+'; //stores operator for cyclical operations
+        Calculadora.pendingVal = displayVal; //stores first operand
+        displayVal = ''; //clears display for next operand input
+        Calculadora.evalArray.push(Calculadora.pendingVal); //pushes first operand into evaluation array
+        Calculadora.evalArray.push('+'); //pushes operator into evaluation array
+        if (displayVal.length > 8) { //validates for 8 digits
           displayVal = displayVal.slice(0, 8);
         }
-        Calculadora.calc_display.innerHTML = displayVal;
+        Calculadora.calc_display.innerHTML = displayVal; //updates value on display as displayVal =''
         break;
 
       case 'menos':
-        Calculadora.currentOperator = '-';
-        Calculadora.pendingVal = displayVal;
-        displayVal = '';
-        Calculadora.evalArray.push(Calculadora.pendingVal);
-        Calculadora.evalArray.push('-');
-        if (displayVal.length > 8) {
+        Calculadora.currentOperator = '-'; //stores operator for cyclical operations
+        Calculadora.pendingVal = displayVal; //stores first operand
+        displayVal = ''; //clears display for next operand input
+        Calculadora.evalArray.push(Calculadora.pendingVal); //pushes first operand into evaluation array
+        Calculadora.evalArray.push('-'); //pushes operator into evaluation array
+        if (displayVal.length > 8) { //validates for 8 digits
           displayVal = displayVal.slice(0, 8);
         }
-        Calculadora.calc_display.innerHTML = displayVal;
+        Calculadora.calc_display.innerHTML = displayVal; //updates value on display as displayVal =''
         break;
 
       case 'por':
-        Calculadora.currentOperator = '*';
-        Calculadora.pendingVal = displayVal;
-        displayVal = '';
-        Calculadora.evalArray.push(Calculadora.pendingVal);
-        Calculadora.evalArray.push('*');
-        if (displayVal.length > 8) {
+        Calculadora.currentOperator = '*'; //stores operator for cyclical operations
+        Calculadora.pendingVal = displayVal; //stores first operand
+        displayVal = ''; //clears display for next operand input
+        Calculadora.evalArray.push(Calculadora.pendingVal); //pushes first operand into evaluation array
+        Calculadora.evalArray.push('*'); //pushes operator into evaluation array
+        if (displayVal.length > 8) { //validates for 8 digits
           displayVal = displayVal.slice(0, 8);
         }
-        Calculadora.calc_display.innerHTML = displayVal;
+        Calculadora.calc_display.innerHTML = displayVal; //updates value on display as displayVal =''
         break;
 
       case 'dividido':
-        Calculadora.currentOperator = '/';
-        Calculadora.pendingVal = displayVal;
-        displayVal = '';
-        Calculadora.evalArray.push(Calculadora.pendingVal);
-        Calculadora.evalArray.push('/');
-        if (displayVal.length > 8) {
+        Calculadora.currentOperator = '/'; //stores operator for cyclical operations
+        Calculadora.pendingVal = displayVal; //stores first operand
+        displayVal = ''; //clears display for next operand input
+        Calculadora.evalArray.push(Calculadora.pendingVal); //pushes first operand into evaluation array
+        Calculadora.evalArray.push('/'); //pushes operator into evaluation array
+        if (displayVal.length > 8) { //validates for 8 digits
           displayVal = displayVal.slice(0, 8);
         }
-        Calculadora.calc_display.innerHTML = displayVal;
+        Calculadora.calc_display.innerHTML = displayVal; //updates value on display as displayVal =''
         break;
 
       case 'igual':
-      if (Calculadora.evalArray.length < 1) {
-        var currentVal = Calculadora.calc_display.innerHTML;
-        Calculadora.evalArray.push(currentVal);
+      if (Calculadora.evalArray.length < 1) { /*evaluates if string evaluation array is empty,
+                                              then it performs cyclical operations with last operator*/
+        var currentVal = Calculadora.calc_display.innerHTML; //takes result from last operation on display
+        Calculadora.evalArray.push(currentVal); 
         Calculadora.evalArray.push(Calculadora.currentOperator);
         Calculadora.evalArray.push(Calculadora.prevVal);
         var result = eval(Calculadora.evalArray.join(' '));
         displayVal = result + '';
         if ((displayVal.length > 8)&&(displayVal.includes('-'))) {
+          displayVal = displayVal.slice(0, 9);
+        }
+        else if ((displayVal.length > 8)&&(displayVal.includes('.'))) {
           displayVal = displayVal.slice(0, 9);
         }
         else {
@@ -134,7 +139,10 @@ var Calculadora = {
         Calculadora.prevVal = displayVal;
         var result = eval(Calculadora.evalArray.join(' '));
         displayVal = result + '';
-        if ((displayVal.length > 8)&&(displayVal.includes('-'))) {
+        if ((displayVal.length > 8)&&(displayVal.includes('-'))) { //validates for 8 digits and sign
+          displayVal = displayVal.slice(0, 9);
+        }
+        else if ((displayVal.length > 8)&&(displayVal.includes('.'))) { //validates for 8 digts and decimal point
           displayVal = displayVal.slice(0, 9);
         }
         else {
@@ -147,12 +155,15 @@ var Calculadora = {
 
       default:
         for (var i = 0; i < numKeyArray.length; i++) {
-          if (clickObj.target.id === numKeyArray[i]) {
-            if (displayVal==='0') {
+          if (clickObj.target.id === numKeyArray[i]) { //validates if key pressed is a number key
+            if (displayVal==='0') { //if number on display is 0, doesn't add another 0
               displayVal = '';
             }
             displayVal += clickObj.target.id;
-            if (displayVal.length > 8) {
+            if ((displayVal.length > 8)&&(displayVal.includes('.'))) { //validates for 8 digits and decimal point
+              displayVal = displayVal.slice(0, 9);
+            }
+            else {
               displayVal = displayVal.slice(0, 8);
             }
             Calculadora.calc_display.innerHTML = displayVal;
@@ -161,7 +172,17 @@ var Calculadora = {
         break;
     } //Switch handler for mouse click events ENDS
   },
+  shrinkBtn: function(clickObj){
+    Calculadora.keyPressed = clickObj.target.id;
+    clickObj.target.style.padding = "2px"; //effect for shrinking button
+    setTimeout(function(){
+      document.getElementById(Calculadora.keyPressed).style.padding = "0px"; //effect for resizing button
+    }, 100);
+  }
 };
 for (var i = 0; i < Calculadora.keyboard.length; i++) {
   Calculadora.keyboard[i].addEventListener('click', Calculadora.updateDisplay, false);
+}
+for (var i = 0; i < Calculadora.keyboard.length; i++) {
+  Calculadora.keyboard[i].addEventListener('click', Calculadora.shrinkBtn, false);
 }
